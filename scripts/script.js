90bc209551a13df8690bc72d888fcf99fc909655
SAlejandr <alejitocastaneda1@gmail.com>
2022-02-16 21:00:41 +0100

    b

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,21 @@ function cambiarModo() {
 
     if (modo == "normal") {
         modo = "dyslexic";
+
         button.innerText = "Normal";
         button.setAttribute("class", "normal-mode");
+
         for (let elemento of elementos) {
             elemento.setAttribute("class", "change dyslexic-mode");
         }
     } else {
         mode = "normal";
+
         button.innerText = "Dyslexic";
         button.setAttribute("class", "dyslexic-mode");
+
         for (let elemento of elementos) {
-            elemento.setAttribute("class", "change");
+            elemento.setAttribute("class", "change normal-mode");
         }
     }
 }
