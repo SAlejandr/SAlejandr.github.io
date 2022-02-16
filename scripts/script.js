ffc93d5280e1ec1372676d525e12487c5ee1da97
SAlejandr <alejitocastaneda1@gmail.com>
2022-02-16 20:58:51 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,30 @@ var modo = "normal";
 var button;
 
 function cambiarModo() {
-  let elementos = document.getElementsByClassName("change");
+    let elementos = document.getElementsByClassName("change");
 
-  if (modo == "normal") {
-    modo = "dyslexic";
-    button.innerText = "Normal";
-    button.setAttribute("class", "normal-mode");
-    for (let elemento of elementos) {
-      elemento.setAttribute("class", "change dyslexic-mode");
+    if (modo == "normal") {
+        modo = "dyslexic";
+        button.innerText = "Normal";
+        button.setAttribute("class", "normal-mode");
+        for (let elemento of elementos) {
+            elemento.setAttribute("class", "change dyslexic-mode");
+        }
+    } else {
+        mode = "normal";
+        button.innerText = "Dyslexic";
+        button.setAttribute("class", "dyslexic-mode");
+        for (let elemento of elementos) {
+            elemento.setAttribute("class", "change");
+        }
     }
-  } else {
-    mode = "normal";
-    button.innerText = "Dyslexic";
-    button.setAttribute("class", "dyslexic-mode");
-    for (let elemento of elementos) {
-      elemento.setAttribute("class", "change");
-    }
-  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  button = document.getElementById("change_button");
+    button = document.getElementById("change_button");
 
-  button.innerText = "Dyslexic";
-  button.setAttribute("class", "dyslexic-mode");
+    button.innerText = "Dyslexic";
+    button.setAttribute("class", "dyslexic-mode");
 
-  button.addEventListener("click", cambiarModo);
+    button.addEventListener("click", cambiarModo);
 });
