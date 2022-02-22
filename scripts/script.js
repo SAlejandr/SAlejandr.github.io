48308549f690700601090f280c6850c658b0a275
SAlejandr <alejitocastaneda1@gmail.com>
2022-02-22 18:09:16 +0100

    probemos

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 var modo = "normal";
-var button;
+var button, modes;
+
+var num = 0;
 
 function cambiarModo() {
     let elementos = document.getElementsByClassName("change");
@@ -9,7 +11,7 @@ function cambiarModo() {
 
         button.innerText = "Normal";
         button.setAttribute("class", "normal-mode");
-
+        modes.setAttribute("class", "dyslexic-mode");
         for (let elemento of elementos) {
             elemento.setAttribute("class", "change dyslexic-mode");
         }
@@ -18,6 +20,7 @@ function cambiarModo() {
 
         button.innerText = "Dyslexic";
         button.setAttribute("class", "dyslexic-mode");
+        modes.setAttribute("class", "normal-mode");
 
         for (let elemento of elementos) {
             elemento.setAttribute("class", "change normal-mode");
@@ -25,8 +28,22 @@ function cambiarModo() {
     }
 }
 
+function chages_modes() {
+    let body = document.getElementById("principal");
+    if(num%2 == 0){
+        
+
+        body.setAttribute("class","container-fluid dark-mode");
+    }else{
+        body.setAttribute("class","container-fluid light-mode");
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     button = document.getElementById("change_button");
+
+    button = document.getElementById("modes");
 
     button.innerText = "Dyslexic";
     button.setAttribute("class", "dyslexic-mode");
